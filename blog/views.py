6236b224ce10dda01ec0052a7f93163d0efc2d8e
Gabriel <3ggramajo@gmail.com>
2015-10-23 16:47:44 -0600

    se agrago un listado para ver los post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Postear
 
-# Create your views here.
+def post_list(request):
+    posts = Postear.objects.filter(Fecha_de_Publicacion__lte=timezone.now()).order_by('Fecha_de_Publicacion')
+    return render(request, 'blog/post_list.html', {'posts': posts})
