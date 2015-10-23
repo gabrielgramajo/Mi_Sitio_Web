2ca80f8c9eb515a481b9d462496f9ba911860470
Gabriel <3ggramajo@gmail.com>
2015-10-23 17:58:38 -0600

    se agrego css para darle estilo

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Postear
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Postear.objects.filter(Fecha_de_Publicacion__lte=timezone.now()).order_by('Fecha_de_Publicacion')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detalle(request, pk):
+    post = get_object_or_404(Postear, pk=pk)
+    return render(request, 'blog/post_detalle.html', {'post': post})
